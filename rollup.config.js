fa2c3feebc65ce34178d3b319413fbccb1a23dbd
Yang Bo <yang7@michaels.coom>
2023-02-01 14:37:44 +0800

    改变dist结构

--- rollup.config.js
+++ rollup.config.js
@@ -1,130 +1,97 @@
-import * as path from 'path'
-import * as fs from 'fs'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
-import postcss from 'rollup-plugin-postcss'
-import json from '@rollup/plugin-json'
-import babel from '@rollup/plugin-babel'
-// import packageJson from './package.json'
-import dts from 'rollup-plugin-dts'
-import sass from 'sass'
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import strip from "@rollup/plugin-strip";
+import typescript from "rollup-plugin-typescript2";
+import path from "path";
+import externals from "rollup-plugin-node-externals";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import sass from "sass";
 
-// 入口
+import pkg from "./package.json";
 
-const entry = 'src/index.ts'
-const componentsDir = 'src/packages'
-const componentsName = fs.readdirSync(path.resolve(componentsDir))
-const componentsEntry = componentsName.map((name) => {
-  console.log(`===============${componentsDir}/${name}/index.ts`);
-  return `${componentsDir}/${name}/index.ts`;
-});
-
-const utilsEntry = 'src/utils/index.ts'
-
-console.log('=============', componentsEntry, '=============')
-
-// 环境变量
-const isProd = process.env.NODE_ENV === 'production'
-const BABEL_ENV = process.env.BABEL_ENV
-
-// Babel配置
-const babelOptions = {
-  presets: ["@babel/preset-env"],
-  extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
-  exclude: "**/node_modules/**"
-}
-
-// 通用插件
-const commonPlugins = [
-  peerDepsExternal(),
-  resolve(),
-  commonjs({ sourceMap: !isProd }),
-  typescript(),
-  babel(babelOptions),
-  json(),
-  // dts()
-]
-
-// 忽略文件
-const externalConfig = [
-  id => /\/__expample__|main.tsx/.test(id),
-  'react',
-  'react-dom',
-  'classname',
-  'react-is',
-  "@fortawesome/fontawesome-svg-core",
-  "@fortawesome/free-solid-svg-icons",
-  "@fortawesome/react-fontawesome",
-  '**/node_modules/**'
-]
-
-// sass打包
 const processScss = function (context) {
   return new Promise((resolve, reject) => {
     sass.compile(
       {
-        file: context
+        file: context,
       },
       function (err, result) {
         if (!err) {
-          resolve(result)
+          resolve(result);
         } else {
-          reject(result)
+          reject(result);
         }
       }
     );
     sass.compile(context, {}).then(
       function (output) {
         if (output && output.css) {
-          resolve(output.css)
+          resolve(output.css);
         } else {
-          reject({})
+          reject({});
         }
       },
       function (err) {
-        reject(err)
+        reject(err);
       }
-    )
-  })
-}
+    );
+  });
+};
 
-// ES Module打包输出
-const esmOutput = {
-  preserveModules: true,
-  // preserveModulesRoot: 'src',
-  // exports: 'named',
-  assetFileNames: ({ name }) => {
-    const { ext, dir, base } = path.parse(name);
-    if (ext !== '.css') return '[name].[ext]';
-    // 规范 style 的输出格式
-    return path.join(dir, 'style', base);
-  }
-}
-
-
-export default () => {
-  return [
-    {
-      input: [entry, ...componentsEntry, utilsEntry],
-      output: { ...esmOutput, dir: 'dist/', format: 'es' },
-      external: externalConfig,
-      plugins: [postcss({
-        extract: true,
-        process: processScss,
-
-      }), ...commonPlugins]
+const dtsFilePathAlias = {
+  typescript: require("ttypescript"),
+  tsconfigDefaults: {
+    compilerOptions: {
+      plugins: [
+        { transform: "typescript-transform-paths" },
+        {
+          transform: "typescript-transform-paths",
+          afterDeclarations: true,
+        },
+      ],
     },
-    {
-      input: [entry, ...componentsEntry, utilsEntry],
-      output: { ...esmOutput, dir: 'dist/type', format: 'es' },
-      external: externalConfig,
-      plugins: [postcss({
+  },
+};
+
+const entry = "src/index.ts";
+
+export default [
+  {
+    input: [entry], // 入口文件
+    output: [
+      {
+        // 出口文件
+        dir: path.dirname(pkg.module),
+        format: "es", // es模块导出，支持按需加载
+        name: pkg.name,
+        exports: "named", // 指定导出模式（自动、默认、命名、无）
+        preserveModules: true, // 保留模块结构
+        preserveModulesRoot: "src", // 将保留的模块放在根级别的此路径下
+      },
+    ],
+    plugins: [
+      // 自动将dependencies依赖声明为 externals
+      externals({
+        devDeps: false,
+      }),
+      peerDepsExternal(),
+      // 处理外部依赖
+      resolve(),
+      // 支持基于 CommonJS 模块引入
+      commonjs(),
+      typescript({
+        // 为了让tsconfig.json配置的路径别名在编译出的.d.ts中转换成相对路径
+        // 例如："@/hooks" => "../../hooks"
+        ...dtsFilePathAlias,
+      }),
+      // 支持 scss
+      postcss({
         extract: true,
         process: processScss,
-
-      }), ...commonPlugins, dts()]
-    }
-  ]
-}
+      }),
+      // 清除调试代码
+      strip(),
+    ],
+  },
+];
